3a3e7c2dfcd6751c7ab8084a485766a6661ace5f
bngahu4-ai <bngahu4@gmail.com>
2026-06-19 14:27:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+const slides = document.querySelectorAll('.slide');
+
+let current = 0;
+
+setInterval(() => {
+
+    slides[current].classList.remove('active');
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    slides[current].classList.add('active');
+
+}, 4000);
+function toggleMenu(){
+document.getElementById("sideMenu").classList.toggle("active");
+}
